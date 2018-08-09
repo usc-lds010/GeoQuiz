49b776379da04d97e9decf1c086e5045a317c1bc
lds010 <lds010@student.usc.edu.au>
2018-08-09 19:52:17 +1000

    Created check answer method and called it

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -43,7 +43,7 @@
                 mTrueButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v){
-                        Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                        checkAnswer(true);
                      }
                 });
 
@@ -52,7 +52,7 @@
                 {
                         @Override
                         public void onClick(View v){
-                        Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                        checkAnswer(false);
                 }
             });
 
@@ -71,6 +71,21 @@
             private void updateQuestion(){
                 int question = mQuestionBank[mCurrentIndex].getTextResId();
                 mQuestionTextView.setText(question);
+            }
+
+            private void checkAnswer(boolean userPressedTrue){
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+                int messageResId = 0;
+
+                if (userPressedTrue == answerIsTrue){
+                    messageResId = R.string.correct_toast;
+                } else{
+                    messageResId = R.string.incorrect_toast;
+                }
+
+                Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+                        .show();
             }
 
         }
